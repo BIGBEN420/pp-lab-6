22a5f5576d7c1398b077f873ae3abaf13e4fbc20
Bartłomiej Krawczyk <65669877+BIGBEN420@users.noreply.github.com>
2024-05-05 11:36:26 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,14 +7,20 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Employee> employees = new ArrayList<>();
         employees.add(new Worker("Wojtek", 10000, 1, "2020-01-10", "Senior Presales"));
-        employees.add(new Worker("Jacek", 12000, 2, "2022-05-13", "Junior Presales"));
+        employees.add(new Worker("Jacek", 12000, 1, "2022-05-13", "Junior Presales"));
         employees.add(new Worker("Artur", 19000, 3, "2012-05-11", "Junior Presales"));
-        employees.add(new Worker("Tomek", 13000, 4, "2015-02-13", "Mid Presales"));
-        employees.add(new Manager("Przemek", 21000, 5, "2010-11-29", "Director Presales"));
-
+        Employee w1 = employees.get(0);
+    
+        employees.add(new Manager("Przemek", 21000, 4, "2010-11-29", "Director Presales"));
+    
         for (Employee e : employees) {
-            e.work();
-            System.out.println("-" + e.getName() + " (ID: " + e.getId() + ", Position: " + e.getPosition() + ", Hire date: " + e.getHireDate() + ", Salary: PLN" + e.getSalary());
+            System.out.println(e.getName() + " has code: " + e.hashCode());
+        }
+    
+        System.out.println("porównywanie w1 o ID:1 z resztą pracowników");
+        for (Employee e : employees) {
+            System.out.println("Comparing " + w1.getName() + " and " + e.getName() + ": " + w1.equals(e));
         }
     }
+    
 }
